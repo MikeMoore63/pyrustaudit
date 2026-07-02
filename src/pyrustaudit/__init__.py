4014ebab0ff0a25a41e9690805d2f29f788d7243
Mike Moore <z_z_zebra@yahoo.com>
2026-07-02 13:44:58 +0100

    feat: remove uneeede and now non existent hooks

--- src/pyrustaudit/__init__.py
+++ src/pyrustaudit/__init__.py
@@ -1,11 +1,8 @@
 from __future__ import absolute_import
 
-from pyrustaudit._pyinstaller import get_hook_dirs, get_PyInstaller_tests
 from pygorpmrustinfo import get_rust_audit
 
 __all__ = [
-    "get_hook_dirs",
-    "get_PyInstaller_tests",
     "get_rust_audit",
 ]
 
